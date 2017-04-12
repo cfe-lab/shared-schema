d71ddce52683e6adcb42054bf8da3d008a04baec
Nat Knight <nknight@cfenet.ubc.ca>
2017-04-12 13:04:37 -0700

    Add index page generate

--- schema/__main__.py
+++ schema/__main__.py
@@ -2,6 +2,7 @@ import sys
 
 import schema.dot as dot
 import schema.data as data
+import schema.index as index
 
 
 def generate():
@@ -9,6 +10,10 @@ def generate():
 
     if cmd == "dot":
         out = dot.make(data.schema_data, "SHARED Schema")
+        print(out)
+        sys.exit(0)
+    elif cmd == "index":
+        out = index.make(data.schema_data)
         print(out)
         sys.exit(0)
     else:
