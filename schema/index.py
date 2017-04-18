fe1b8e27d3e8f21197770eee7e6d2c1bbb5ac2e2
Nat Knight <nknight@cfenet.ubc.ca>
2017-04-18 16:59:47 -0700

    Add datatype description and schema generation date

--- schema/index.py
+++ schema/index.py
@@ -1,3 +1,4 @@
+import datetime
 import string
 
 import schema.data as data
@@ -6,32 +7,76 @@ import schema.entity as entity
 
 html5_tmpl = string.Template('''<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="style.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
-    <title>$title</title>
-  </head>
-  <body>
-    <a name="top"></a>
-    <h1>SHARED Schema</h1>
-    <p>Draft version</p>
-    <h2>Schema</h2>
-    <div id="schema">
-      <embed type="image/svg+xml" src="schema.svg"></embed>
-      <p><a href="schema.svg" target="_blank">bigger</a></p>
-    </div>
-    <h2>Data Dictionary</h2>
-    <p>In no particular order, the fields associated with each table.</p>
-    $entities
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
+        <title>$title</title>
+    </head>
+    <body>
+        <a name="top"></a>
+        <h1>SHARED Schema</h1>
+        <p>Draft version ($date)</p>
+        <h2>Schema</h2>
+        <div id="schema">
+            <embed type="image/svg+xml" src="schema.svg"></embed>
+            <p><a href="schema.svg" target="_blank">bigger</a></p>
+        </div>
+        <h2>Data Dictionary</h2>
+        <p>The fields associated with each table. The following data-types are allowed:</p>
+
+        <table>
+            <tr>
+                <th>Data Type</th>
+                <th>Description</th>
+            </tr>
+
+            <tr>
+                <td>integer</td>
+                <td>A whole number.</td>
+            </tr>
+            <tr>
+                <td>float</td>
+                <td>A decimal number.</td>
+            </tr>
+            <tr>
+                <td>string</td>
+                <td>A piece of text.</td>
+            </tr>
+            <tr>
+                <td>date</td>
+                <td>A calendar date (YYYY-MM-DD).</td>
+            </tr>
+            <tr>
+                <td>uuid</td>
+                <td>An arbitrary unique identifier (e.g: 'cf66f56d-5bd0-477c-bca9-0fb712cf8753').</td>
+            </tr>
+            <tr>
+                <td>bool</td>
+                <td>True or False.</td>
+            </tr>
+            <tr>
+                <td>enum (...)</td>
+                <td>Once of a given set of values. For example, enthnicity might be represented by the data-type 'enum(caucasian, latino, asian, black, indigenous-american)'. An '...' indicates that the allowed values are to-be-determined.</td>
+            </tr>
+            <tr>
+                <td>foreign key (tbl_name)</td>
+                <td>Indicates a link to a different table. For example, viral isolate's record might include a 'foreign key (Person)' field to link the isolate to the participant who gave it.</td>
+            </tr>
+        </table>
+
+        <hr/>
+
+        $entities
+    </body>
 </html>''')
 
 
 def index(title, entities):
-    return html5_tmpl.substitute(title=title, entities=entities)
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    return html5_tmpl.substitute(title=title, entities=entities, date=date)
 
 
 # Create entries
