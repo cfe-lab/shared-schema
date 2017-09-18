af6ab43797c5c78a521a8e2cfb518be768cc8e44
Nathaniel Knight <nathaniel.ep@gmail.com>
2017-09-18 09:56:39 -0700

    Add written description of submission schemes

---
+++ shared_schema/submission_schemes/__init__.py
@@ -0,0 +1,6 @@
+'''Definition and export for data submission schemes.
+
+Submission schemes are simplified subsets of the schema that collaborators can
+use as a guide when submitting data. Schemes are defined as "views" onto the
+schema so that names, types, and descriptions aren't duplicated.
+'''
