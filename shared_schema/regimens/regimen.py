d3fc894dc05efdf4e91803ef7cc3c19aa8e7f42a
Nathaniel Knight <nknight@cfenet.ubc.ca>
2017-11-09 17:19:33 -0800

    Add a Regimen object for representing data
    This commit adds a Regimen data object that's cannonical, hashable,
    and equable. Specifically:
    
    - It will combine Doses, Drug Lists, and Regimen Parts as much as
      possible when constructing an instance.
    - It's composed of strings, decimals, frozensets, and (named)tuples,
      so it's got solid equality and hashing operations for free, and is
      mostly immutable.
    
    The implementation of cannonicalization is a little special, so it's
    explicitly documented.

---
+++ shared_schema/regimens/regimen.py
@@ -0,0 +1,256 @@
+'''A hashable, eq'able, cannonical representation of drug regimens'''
+import collections
+import decimal
+import functools
+
+from . import grammar
+
+# NOTE(nknight): We don't consider grammar.TimeUnit or grammar.Number
+# because we cannonicalise the things that contain them direcly
+# (Durations and Amounts); they're required for parsing, but don't
+# contain extra information.
+
+
+# ---------------------------------------------------------------------
+# Helpers
+
+def types_match(f):
+    "Ensures that the types of the decorated binary function's arguments match"
+    @functools.wraps(f)
+    def wrapped(x, y):
+        if type(x) is not type(y):
+            msg = "Can't combine '{}' and '{}"
+            raise ValueError(msg.format(x, y))
+        return f(x, y)
+    return wrapped
+
+
+# ---------------------------------------------------------------------
+# Cannonicalizing Collections
+'''These functions operate on collections and objects that can
+*sometimes* be consolidated, e.g. Doses of the same compound.
+
+- `key` accesses the attribute of an object that must match in order
+  to consolidate
+- `add` combines to objects that can be consolidated
+'''
+
+_dose = collections.namedtuple('Dose', ['amount', 'compound'])
+_indication = collections.namedtuple('Indication', ['doselist', 'frequency'])
+_regimen_part = collections.namedtuple(
+    'RegimenPart',
+    ['drug_combination', 'duration'],
+)
+
+
+@functools.singledispatch
+def key(x):
+    '''Consolidatable types with the same key can be consolidated.
+
+    E.g: For a Dose, the key is the compound type. Doses of the same
+    compound can be consolidated, but doses of different compounds
+    must be kept separate.
+    '''
+    msg = "key: {}"
+    raise NotImplemented(msg.format(x))
+
+
+@functools.singledispatch
+def add(x, other):
+    '''Binary operation for combining two consolidatable types that have
+    the same key.
+
+    E.g: For two Doses with the same compound, return a new dose with
+    the same compound, adding the amounts together.
+    '''
+    msg = "add: {}"
+    raise NotImplemented(msg.format(x))
+
+
+# TODO(nknight): Hypothesis testing for this function's postconditions:
+#  - each `key` value is unique
+#  - total for each `key` is the same before and after
+#  - etc.
+
+def consolidating_insert(collection, newobj):
+    '''Insert an object into a collection, consolidating if possible.
+
+    Implemented in terms of `key` and `add` and iterability of
+    collection.
+
+    Returns a new collection (specifically, a frozenset) of objects
+    with the given newobj added. If any objects in this new collection
+    have a `key` value that matches newobj, they will all be
+    consolidated in the new collection.
+
+    The collection should contain only instances of the same class as
+    newobj.
+    '''
+    maybe_obj = next(iter(collection), None)
+    if maybe_obj is not None:
+        if type(maybe_obj) is not type(newobj):
+            msg = "Can't insert {} into a set of {}"
+            raise ValueError(msg.format(newobj, maybe_obj))
+    matches = [obj for obj in collection if key(obj) == key(newobj)]
+    others = [obj for obj in collection if key(obj) != key(newobj)]
+    consolidated = functools.reduce(add, matches, newobj)
+    return frozenset([consolidated] + others)
+
+
+def consolidating_merge(xs, ys):
+    return functools.reduce(consolidating_insert, xs, ys)
+
+
+def consolidate(xs):
+    return consolidating_merge(xs, [])
+
+
+@key.register(_dose)
+def _(dose):
+    return dose.compound
+
+
+@add.register(_dose)
+@types_match
+def _(dose, other):
+    if key(dose) != key(other):
+        msg = "Can't add doses of different compounds: {} + {}"
+        raise ValueError(msg.format(dose, other))
+    return _dose(
+        compound=dose.compound,
+        amount=dose.amount + other.amount,
+    )
+
+
+@key.register(_indication)
+def _(indication):
+    return indication.frequency
+
+
+@add.register(_indication)
+@types_match
+def _(indication, other):
+    if key(indication) != key(other):
+        msg = "Can't add indications of different frequencies: {} + {}"
+        raise ValueError(msg.format(indication, other))
+    new_doselist = consolidating_merge(indication.doselist, other.doselist)
+    return _indication(
+        doselist=new_doselist,
+        frequency=indication.frequency,
+    )
+
+
+@key.register(_regimen_part)
+def _(regimen_part):
+    return regimen_part.duration
+
+
+@add.register(_regimen_part)
+@types_match
+def _(regimen_part, other):
+    if key(regimen_part) != key(other):
+        msg = "Can't add regimen parts with different durations: {} + {}"
+        raise ValueError(msg.format(regimen_part, other))
+    new_drug_combination = consolidating_merge(
+        regimen_part.drug_combination,
+        other.drug_combination,
+    )
+    return _regimen_part(
+        duration=regimen_part.duration,
+        drug_combination=new_drug_combination,
+    )
+
+
+# ---------------------------------------------------------------------
+# Converter
+
+@functools.singledispatch
+def parse(src):
+    '''Parse a grammar object into a hashable, eq'able object
+
+    This multi-method should turn each node that's parsed by the
+    regimen grammar into a tuple or frozenset of builtin scalar
+    datatypes. This ensures that two identical regimens will parse to
+    the same object, even when they're expressed differently.
+    '''
+    msg = "Unhandled object of type '{} : '{}'"
+    raise ValueError(msg.format(type(src), src))
+
+
+@parse.register(grammar.Frequency)
+def _(src):
+    return str(src)
+
+
+@parse.register(grammar.Compound)
+def _(src):
+    return str(src)
+
+
+@parse.register(grammar.Amount)
+def _(src):
+    return src.milligrams
+
+
+@parse.register(grammar.Dose)
+def _(src):
+    amount, compound = src
+    return _dose(amount=parse(amount), compound=parse(compound))
+
+
+@parse.register(grammar.DoseList)
+def _(src):
+    doses = [parse(dose) for dose in src]
+    return frozenset(consolidate(doses))
+
+
+@parse.register(grammar.Indication)
+def _(src):
+    doselist, frequency = src
+    return _indication(
+        doselist=parse(doselist),
+        frequency=parse(frequency),
+    )
+
+
+@parse.register(grammar.Duration)
+def _(src):
+    days = src.days
+    return decimal.Decimal(days)
+
+
+@parse.register(grammar.DrugCombination)
+def _(src):
+    indications = consolidate(parse(ind) for ind in iter(src))
+    return frozenset(indications)
+
+
+@parse.register(grammar.RegimenPart)
+def _(src):
+    drug_combination, duration = src
+    return _regimen_part(
+        drug_combination=parse(drug_combination),
+        duration=parse(duration),
+    )
+
+
+@parse.register(grammar.Regimen)
+def _(src):
+    parts = consolidate(parse(part) for part in iter(src))
+    return frozenset(parts)
+
+
+# ---------------------------------------------------------------------
+
+class Regimen(object):
+    '''A hashable, eq'able object representing a drug regimen.'''
+
+    def __init__(self):
+        pass
+
+    def __hash__(self):
+        pass
+
+    @classmethod
+    def from_grammar_object(cls, grammar_obj):
+        return parse(grammar_obj)
---
+++ test/regimens/test_regimen.py
@@ -0,0 +1,216 @@
+'''Test the hashable, equable, cannonical regimen data object'''
+
+import decimal
+import unittest
+
+import pypeg2 as pp
+
+import shared_schema.regimens.grammar as rg
+import shared_schema.regimens.regimen as regimen
+
+
+# ---------------------------------------------------------------------
+# Helpers
+
+def methods_exist(obj):
+    '''Verify that __eq__ and __hash__ exist on an object'''
+    method_names = ('__eq__', '__hash__')
+    obj_methods = dir(obj)
+    msg = "Expected '{}' to have {} method"
+    msg.format(obj, ", ".join(method_names))
+    return (all(nm in obj_methods for nm in method_names), msg)
+
+
+def equivalent(a, b):
+    '''Verify that two objects are equal and have the same hash'''
+    if a != b:
+        msg = "Expected {} == {}"
+        return (False, msg.format(a, b))
+    if hash(a) != hash(b):
+        msg = "Expected hash({}) == hash({})"
+        return (False, msg.format(a, b))
+    return True, ""
+
+
+def parse_from_source(src, grammar_node):
+    grammar_obj = pp.parse(src, grammar_node)
+    parsed = regimen.parse(grammar_obj)
+    return parsed
+
+
+# ---------------------------------------------------------------------
+# Example Data
+dcv_dose = regimen._dose(amount=decimal.Decimal(1), compound='DCV')
+sof_dose = regimen._dose(amount=decimal.Decimal(2), compound='SOF')
+boc_dose = regimen._dose(amount=decimal.Decimal(3), compound='BOC')
+
+duration_12wks = decimal.Decimal(12 * 7)
+duration_24wks = decimal.Decimal(24 * 7)
+
+
+# ---------------------------------------------------------------------
+# Test Cases
+
+class TestParsingCompoundObjects(unittest.TestCase):
+
+    def test_dose(self):
+        src = "1mg DCV"
+        parsed = parse_from_source(src, rg.Dose)
+        self.assertTrue(*methods_exist(parsed))
+        self.assertEqual(parsed, dcv_dose)
+
+    def test_indication(self):
+        src = "1mg DCV + 2mg SOF QD"
+        parsed = parse_from_source(src, rg.Indication)
+        self.assertTrue(*methods_exist(parsed))
+        self.assertEqual(
+            parsed,
+            regimen._indication(
+                frequency="QD",
+                doselist=frozenset([dcv_dose, sof_dose]),
+            ),
+        )
+
+    def test_regimenpart(self):
+        src = "2mg SOF QD 12 weeks"
+        parsed = parse_from_source(src, rg.RegimenPart)
+        self.assertTrue(*methods_exist(parsed))
+        self.assertEqual(
+            parsed,
+            regimen._regimen_part(
+                duration=duration_12wks,
+                drug_combination=frozenset([
+                    regimen._indication(
+                        frequency='QD',
+                        doselist=frozenset([sof_dose]),
+                    )
+                ]),
+            )
+        )
+
+
+class TestParsingCollections(unittest.TestCase):
+
+    def test_doselist_parses(self):
+        src = "1mg DCV + 3mg BOC"
+        parsed = parse_from_source(src, rg.DoseList)
+        self.assertIsInstance(parsed, frozenset)
+        self.assertTrue(*methods_exist(parsed))
+        self.assertEqual(
+            parsed,
+            frozenset([dcv_dose, boc_dose]),
+        )
+
+    def test_doselist_cannonical(self):
+        dl1 = parse_from_source("2mg DCV", rg.DoseList)
+        dl2 = parse_from_source("1mg DCV + 1mg DCV", rg.DoseList)
+        self.assertTrue(*methods_exist(dl1))
+        self.assertTrue(*methods_exist(dl2))
+        self.assertTrue(*equivalent(dl1, dl2))
+
+    def test_drugcombination(self):
+        parsed = parse_from_source(
+            "1mg DCV QD & 3mg BOC TID",
+            rg.DrugCombination,
+        )
+        self.assertIsInstance(parsed, frozenset)
+        self.assertTrue(*methods_exist(parsed))
+        self.assertEqual(
+            parsed,
+            frozenset([
+                regimen._indication(
+                    doselist=frozenset([dcv_dose]),
+                    frequency="QD",
+                ),
+                regimen._indication(
+                    doselist=frozenset([boc_dose]),
+                    frequency="TID",
+                ),
+            ]),
+        )
+
+    def test_regimen(self):
+        parsed = parse_from_source(
+            "(1mg DCV + 2mg SOF) QD 12 week, 3mg BOC QD 24 weeks",
+            rg.Regimen,
+        )
+        self.assertIsInstance(parsed, frozenset)
+        self.assertTrue(*methods_exist(parsed))
+        self.assertEqual(
+            parsed,
+            frozenset([
+                regimen._regimen_part(
+                    duration=duration_12wks,
+                    drug_combination=frozenset([
+                        regimen._indication(
+                            frequency='QD',
+                            doselist=frozenset([dcv_dose, sof_dose]),
+                        )
+                    ]),
+                ),
+                regimen._regimen_part(
+                    duration=duration_24wks,
+                    drug_combination=frozenset([
+                        regimen._indication(
+                            frequency='QD',
+                            doselist=frozenset([boc_dose]),
+                        )
+                    ]),
+                )
+            ]),
+        )
+
+
+class TestMonoidalOperations(unittest.TestCase):
+
+    # NOTE(nknight): Skip testing regimen.key, as it's just an accessor
+
+    dose_a1 = regimen._dose(amount=1, compound='test')
+    dose_a2 = regimen._dose(amount=2, compound='test')
+    dose_b1 = regimen._dose(amount=1, compound='other test')
+
+    druglist_a = frozenset([dose_a1])
+    druglist_b = frozenset([dose_b1])
+    druglist_ab = frozenset([dose_a1, dose_b1])
+
+    def test_dose_add(self):
+        self.assertEqual(
+            regimen.add(self.dose_a1, self.dose_a2),
+            regimen._dose(amount=3, compound='test'),
+        )
+        with self.assertRaises(ValueError):
+            regimen.add(self.dose_a1, self.dose_b1)
+
+    def test_dose_consolidating_insert(self):
+        xs = frozenset([self.dose_a1, self.dose_b1])
+        new_xs = regimen.consolidating_insert(xs, self.dose_a1)
+        self.assertEqual(
+            new_xs,
+            frozenset([
+                regimen._dose(amount=2, compound='test'),
+                self.dose_b1
+            ]),
+        )
+
+    def test_indication_consolidating_insert(self):
+        self.assertEqual(
+            regimen.consolidating_insert(
+                self.druglist_a,
+                self.dose_b1,
+            ),
+            self.druglist_ab,
+        )
+        self.assertEqual(
+            regimen.consolidating_insert(
+                self.druglist_a,
+                self.dose_a1,
+            ),
+            frozenset([self.dose_a2]),
+        )
+        self.assertEqual(
+            regimen.consolidating_insert(
+                self.druglist_ab,
+                self.dose_a1,
+            ),
+            frozenset([self.dose_a2, self.dose_b1]),
+        )
